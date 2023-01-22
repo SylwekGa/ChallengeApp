b121245d156c33fb9f0a4b39f78e37f7aed0e694
Sylwek <sylwek.gajda@gmail.com>
2023-01-22 10:41:45 +0100

    zadanie dzień 4

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,18 +1,20 @@
 ﻿string imię = "Ewa";
 string płeć = "kobieta";
-var wiek = 33;
+var wiek = 17;
 
 if (płeć == "kobieta" && wiek < 30)
 {
-   Console.WriteLine("Kobieta poniżej 30 lat");
+    Console.WriteLine("Kobieta poniżej 30 lat");
 }
-else if (płeć == "kobieta" && imię == "Ewa")
+else if (wiek == 33 && imię == "Ewa")
 {
     Console.WriteLine("Ewa, lat 33");
 }
 else if (płeć != "kobieta" && wiek < 18)
-   Console.WriteLine("Niepełnoletni mężczyzna");
-else 
+{
+    Console.WriteLine("Niepełnoletni mężczyzna");
+}
+     else
 {
     Console.WriteLine("Pełnoletnia kobieta");
 }
